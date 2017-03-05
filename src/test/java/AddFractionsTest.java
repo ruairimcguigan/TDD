c5d5254be2a17a502e1cfac8d78435b13633a83e
Ruairi <ruairi.mcguigan@contractor.isbanuk.com>
2017-03-05 10:32:10 +0000

    NegativeInputsAndNegativeOutputs test added

--- src/test/java/AddFractionsTest.java
+++ src/test/java/AddFractionsTest.java
@@ -16,9 +16,27 @@ public class AddFractionsTest {
 
     @Test
     public void nonZeroPlusZero() throws Exception {
-
         Fraction sum = new Fraction(3).plus(new Fraction(0));
         assertEquals(3, sum.intValue());
+    }
+
+    @Test
+    public void zeroPlusNonZero() throws Exception {
+        Fraction sum = new Fraction(0).plus(new Fraction(5));
+        assertEquals(5, sum.intValue());
+    }
+
+    @Test
+    public void nonZeroPlusNonNegativeOperands() throws Exception {
+
+        Fraction sum = new Fraction(3).plus(new Fraction(4));
+        assertEquals(7, sum.intValue());
+    }
+
+    @Test
+    public void negativeInputsAndNegativeOutputs() throws Exception {
+        Fraction sum = new Fraction(-3).plus(new Fraction(1));
+        assertEquals(-2, sum.intValue());
 
     }
 }
